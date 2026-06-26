b713b0610737247422f5345cc8d12b56186eb7a5
Ethan Sloan <ethanjohnsloan@gmail.com>
2026-06-26 10:57:22 -0700

    Initial commit

---
+++ src/main.f
@@ -0,0 +1,30 @@
+      PROGRAM ISING
+C
+C Driver program for a very simple 2D Ising model!
+C
+      INCLUDE 'ising.inc'
+C
+      INTEGER STEP
+      REAL E
+      REAL M
+C
+C Initialize
+C
+      CALL INIT
+C
+C Monte Carlo loop
+C
+      DO 100 STEP=1,NSTEPS
+C
+       CALL SWEEP
+C
+       CALL ENERGY(E)
+C
+       CALL MAGNETIZATION(M)
+C
+       CALL OUTPUT(STEP,E,M)
+C
+ 100  CONTINUE
+C
+      STOP
+      END
